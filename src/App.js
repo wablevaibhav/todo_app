6dc17fca6c60016badca9f5cc3070c0a3e5ab81a
wablevaibhav <vaibhavwabale@hotmail.com>
2023-03-01 18:00:00 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import { Button } from '@mui/material';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]); 
+  const [input, setInput] = useState('');
+
+  const addTodo = (event) => {
+    // this will fire off when we click the button
+    event.preventDefault(); //will stop the refresh
+    setTodos([...todos, input]);
+    console.log(setTodos);
+    setInput(''); //clear up input after clicking add todo button
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Coders !!</h1>
+      <form>
+      <input value={input} onChange={event => setInput(event.target.value)}/>
+      <Button type="submit" onClick={addTodo} variant="contained">Add Todo</Button>
+      </form>
+      <ul>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
